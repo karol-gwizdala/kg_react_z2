7ef67c492f5a8870f23a4f2ac5c812490b6ca9a4
karol-gwizdala <gwizdala.karol@gmail.com>
2023-11-01 11:16:54 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,30 @@
+import useState from 'react';
 import './App.scss';
 
 
-const Quote = () => {
- interface Quote {
+
+interface Quote {
   text: string,
   authot: string
- } 
+ }
+
+const quote_list = ["Ludzie budują za dużo murów, a za mało mostów.", "To smutne, że głupcy są tacy pewni siebie, a ludzie rozsądni tacy pełni wątpliwości.", "Rób, co możesz, w miejscu, jakim jesteś i z tym, co masz."]
+
+const Quote = () => {
+
+  // const [currentQuote, setCurrentQuote] = useState (Quote[0]);
+  
+  const handleGenerateQuoteClick = Math.floor(Math.random() * quote_list.length);
+  return (
+    <div>{quote_list[handleGenerateQuoteClick]}</div>
+  )
+
 }
 
 function App() {
   return (
     <main className="main">
-      {/*  */}
+      <Quote />
       </main>
   );
 }
